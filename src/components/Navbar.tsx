098e298668725c39df703e2a4434ed48eeb4309a
unknown <mennahisham1d@gmail.com>
2023-08-31 13:20:19 +0200

    error pg & navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,19 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
   
-    <nav className='bg-blue-900 text-white '>
-        <NavLink to="/">store</NavLink>
-        <NavLink to="/cart">cart</NavLink>
+    <nav className='bg-blue-900 text-white  sticky
+
+    '>
+      <div className="flex justify-center gap-5 container  mx-auto sm:px-10 md:px-20 lg:px-32  border border-green-500">
+      <NavLink to="/" className="capitalize p-3 hover:bg-blue-600">store</NavLink>
+        <NavLink to="/cart" className="capitalize p-3 hover:bg-blue-600">cart</NavLink>
+
+
+      </div>
+     
+   
+     
+        
 
     </nav>
   )
