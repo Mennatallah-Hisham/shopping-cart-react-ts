7758273bf08e1d606de31a58070d8b84a94b7c0b
unknown <mennahisham1d@gmail.com>
2023-09-10 15:41:50 +0200

    final

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <section className=" bg-blue-900 text-white  sticky top-0" >
       <div className="container flex  justify-between items-center">
-      <p className="capitalize text-lg"> modren home</p>
+      <p className="capitalize text-lg"> lines</p>
       <nav className="flex  gap-5  sm:px-10 md:px-20 lg:px-32 ">
   <NavLink to="/" className="capitalize p-3 font-medium text-base hover:bg-blue-600">store</NavLink>
     <NavLink to="/cart" className="capitalize p-3 font-medium text-base hover:bg-blue-600">
